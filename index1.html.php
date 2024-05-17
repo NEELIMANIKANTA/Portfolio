3b079cfc42b471a74fb93b920bd093b235ffab87
NEELI-MANIKANTA <113284432+NEELI-MANIKANTA@users.noreply.github.com>
2024-05-17 16:24:36 +0530

    my portfolio

---
+++ index1.html.php
@@ -0,0 +1,37 @@
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+<h2 style="text-align:center;margin-top:50px;">NEELI MANIKANTA</h2>
+<br/>
+<br/>
+<br/>
+<h1>Enter for Student Name, Roll NO, Branch, Address </h1>
+<form  method="post">
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Name</label>
+  <input type="text" name="name" class="form-control" id="exampleFormControlInput1" placeholder="Name">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Roll Number</label>
+  <input type="text" name="rno" class="form-control" id="exampleFormControlInput1" placeholder="Roll Number">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Branch</label>
+  <input type="text" name="bran" class="form-control" id="exampleFormControlInput1" placeholder="Branch">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Address</label>
+  <input type="text" name="add" class="form-control" id="exampleFormControlInput1" placeholder="Address">
+</div>
+<input class="btn btn-primary" type="submit" name="submit" />
+</form>
+</div>
+<!--- php code--->
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
